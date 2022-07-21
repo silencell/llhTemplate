a122e8d2144c3df07d253bbb00441ced80606ce5
GitHub Action <action@github.com>
2022-07-21 12:34:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/silencell/llhtemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/silencell/llhtemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.silencell.llhtemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.silencell.llhtemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/silencell/llhtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/silencell/llhtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.silencell.llhtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.silencell.llhtemplate.MyBundle
 
 class MyApplicationService {
 
